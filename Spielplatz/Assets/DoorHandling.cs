ba38a4f42cba7bbe3ce77d63195dc4057c338d02
fintis00 <finn.tissen@alumni.fh-aachen.de>
2022-11-10 21:38:43 +0100

    DoorOpen

---
+++ Spielplatz/Assets/DoorHandling.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DoorHandling : MonoBehaviour
+{
+    public Transform cam;
+    [SerializeField]
+    public GameObject door;
+
+    [SerializeField]
+    private AudioClip doorSound;
+    public float volume;
+
+    bool isOpen = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isOpen)
+        {
+            LeanTween.moveLocalX(door, -8, 3);
+            AudioSource.PlayClipAtPoint(doorSound, cam.position, volume);
+            isOpen = true;
+            Invoke(nameof(CloseDoor), 7);
+            }
+    }
+
+    private void CloseDoor()
+    {
+        LeanTween.moveLocalX(door, -5, 3);
+        isOpen = false;
+    }
+    
+}
